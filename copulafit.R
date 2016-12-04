3f3e46508413b3a71227d489a64ae589a697354a
helderrezende <helderfgv@gmail.com>
2016-12-04 16:17:58 -0200

    adicionando a parte em R

---
+++ copulafit.R
@@ -0,0 +1,13 @@
+
+
+library(copula)
+library(zoo)
+
+uniform <- read.zoo("C:\\Users\\g4u55\\Downloads\\TCC-master\\TCC-master\\uniforms.csv",header = TRUE, sep = ",")
+uniform = as.matrix(uniform)
+set.seed(500)
+tcop <- tCopula(dim = 4,dispstr = "un")
+fit.t <- fitCopula(tcop, uniform, method="ml")
+write.csv(coef(fit.t), file = "c://ParametrosOtimo.csv")
+u <- rCopula(3965,tCopula(dim=4,dispstr = "un",coef(fit.t)[1:6],df=coef(fit.t)[7]))
+write.csv(u, file = "c://CopulaOtimaSimulada.csv")
